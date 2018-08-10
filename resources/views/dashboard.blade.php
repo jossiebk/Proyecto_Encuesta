bc2d8e79844c85cb2ccf6318eca4787597685f8a
salaz310 <lrso.10@hotmail.com>
2018-08-10 15:17:20 -0500

    Reordene el dashboard y puse fondo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,19 @@
 
 
 @section('content')
+<div style="background-image: url(img/header.jpg); height: 1250px; width: 1400px;">
 
-<section class="p-0" id="top">
-  
+      <div class="row align-items-center justify-content-center">
+          <h1 style="color:transparent;" class="display-1">e</h1>
+      </div>
+      <div class="row align-items-center justify-content-center">
+          <h1 style="color:white;" class="display-4">Bienvenido</h1>
+      </div>
 
-</section>
 
-
-  <section class="p-0" id="portfolio">
-      <div class="container-fluid p-0">
-        <div class="row no-gutters">
+      <div class="row align-items-center justify-content-center">
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{ url('/') }}">
+            <a class="portfolio-box" href="#">
               <img class="img-fluid" src="img/teachers.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
@@ -27,9 +28,12 @@
               </div>
             </a>
           </div>
+      </div>
 
+
+      <div class="row align-items-center justify-content-center">
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="">
+            <a class="portfolio-box" href="#">
               <img class="img-fluid" src="img/taux.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
@@ -43,25 +47,25 @@
               </div>
             </a>
           </div>
+      </div>
+      
 
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
-              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+      <div class="row align-items-center justify-content-center">
+          <div class="col-lg-4 col-sm-6 ">
+            <a class="portfolio-box" href="#">
+              <img class="img-fluid" src="img/buscar.jpg" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
                     Buscar
                   </div>
                   <div class="project-name">
-                    Project Name
+                    Busca algun catedratico o auxiliar
                   </div>
                 </div>
               </div>
             </a>
           </div>
-
-          
-        </div>
-      </div>
-    </section>
+     </div>
+ </div>
 @endsection
