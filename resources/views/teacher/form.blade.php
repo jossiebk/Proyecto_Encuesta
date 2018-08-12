9f70fdc95999dec5ca8df23324999c213e7c9c7a
Esteban Alvarez <davix93a@gmail.com>
2018-08-11 22:11:55 -0600

    Aplicacion de Bootstrap y CSS al formulario de docente

--- resources/views/teacher/form.blade.php
+++ resources/views/teacher/form.blade.php
@@ -1,60 +1,94 @@
 @extends('layouts.app')
 
 @section('title', 'Encuesta Profesor')
-	<header class="masthead text-center text-white">
-		<br><br><br><br><br><br><br><br>
+<div style="background-image: url(img/header.jpg); height: 1250px; width: 1400px;">
 
-		<br><br>
-		{!! Form::open(['url' => '/teacher_form/calculate']) !!}
- 			{!!Form::label('Pregunta1', '1. Pregunta 1?')!!}
- 			<br>
- 			{!!Form::number('P1', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta2', '2. Pregunta 2?')!!}
- 			<br>
- 			{!!Form::number('P2', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta3', '3. Pregunta 3?')!!}
- 			<br>
- 			{!!Form::number('P3', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta4', '4. Pregunta 4?')!!}
- 			<br>
- 			{!!Form::number('P4', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta5', '5. Pregunta 5?')!!}
- 			<br>
- 			{!!Form::number('P5', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta6', '6. Pregunta 6?')!!}
- 			<br>
- 			{!!Form::number('P6', '10', ['max'=> 10, 'min'=>0])!!} 
- 			<br><br>	
- 			{!!Form::label('Pregunta7', '7. Pregunta 7?')!!}
- 			<br>
- 			{!!Form::number('P7', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta8', '8. Pregunta 8?')!!}
- 			<br>
- 			{!!Form::number('P8', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta9', '9. Pregunta 9?')!!}
- 			<br>
- 			{!!Form::number('P9', '10', ['max'=> 10, 'min'=>0])!!}
- 			<br><br>
- 			{!!Form::label('Pregunta10', '10. Pregunta 10?')!!}
- 			<br>
- 			{!!Form::number('P10', '10', ['max'=> 10, 'min'=>0])!!} 	 					 			 	
- 			<br><br>
- 			<br><br>
- 			{!!Form::label('Opinion1', 'Opinion 1.')!!}
- 			<br><br>
- 			{!!Form::textarea('O1', null, ['rows' => 5, 'cols' => 30,'style' => 'resize:none'])!!}
- 			<br>
- 			{!!Form::submit('Evaluar')!!}
+
+
+		<br><br><br><br>
+		<!-- Formulario de preguntas dedicadas a la evaluacion del catedratico-->
+
+		{!! Form::open(['url' => '/teacher_form/calculate', 'class' => 'form-row align-items-center']) !!}
+		<div class="container" type="text" readonly>
+
+
+			<div class="container">
+
+
+		<div class="card">
+  		<div class="card-header">
+    		Instrucciones:
+  		</div>
+  		<div class="card-body">
+    		<h5 class="card-title">Evaluacion de docente</h5>
+    		<p class="card-text">Responda las preguntas que a continuacion se le presentan. Seleccione valores dentro de 1 a 10 siendo 1 malo y 10 bueno.</p>
+  		</div>
+		</div>
+		<BR><BR>
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta1', '1. Pregunta 1?' ,array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P1', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), '', array('class' => 'custom-select mr-sm-2' ))!!}
+
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta2', '2. Pregunta 2?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P2', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+ 	
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta3', '3. Pregunta 3?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P3', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+ 
+
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta4', '4. Pregunta 4?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P4', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+			
+<!--******************************************************************-->
+ 		{!!Form::label('Pregunta5', '5. Pregunta 5?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P5', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta6', '6. Pregunta 6?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P6', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+ 	
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta7', '7. Pregunta 7?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P7', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!} 
+
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta8', '8. Pregunta 8?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P8', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+
+		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta9', '9. Pregunta 9?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P9', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+ 
+
+ 		<!--******************************************************************-->
+ 		{!!Form::label('Pregunta10', '10. Pregunta 10?',array('class' => 'text-white'))!!}
+ 		<br>
+		{!!Form::select('P10', array('N1' => '1', 'N2' => '2','N3' => '3', 'N4' => '4','N5' => '5', 'N6' => '6','N7' => '7', 'N8' => '8','N9' => '9', 'N10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
+
+		<br><br><br><br>
+ 			{!!Form::submit('Evaluar', array('class'=>'btn btn-primary btn-lg btn-block'))!!}
+ 		</div>
 		{!! Form::close() !!}
-
-	</header>
+	</div>
+	</div>
 
 @section('content')
 
