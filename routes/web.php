796989cac1a739889ce674c0d26c2b77e80d381b
Jossie Castrillo <bkcastrillo@gmail.com>
2018-08-12 20:29:10 -0600

    correccion de error en rutas

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,9 @@ Route::get('/dash', function () {
 });
 
 Route::get('/teacher_form','TeacherController@index');
-
-
-
-
-
-
-
-
-
-
-
-
+Route::post('/teacher_form/evaluate','TeacherController@store');
 
 // ruta para perfil del catedratico
 Route::get('/perCat', function () {
     return view('catProfile');
 });
-=======
-Route::post('/teacher_form/evaluate','TeacherController@store');
->>>>>>> b71af5637b28152e492bfa6a38d196b49f0c8a4b
