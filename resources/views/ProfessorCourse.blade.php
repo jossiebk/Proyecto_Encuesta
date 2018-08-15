fcc379fca1901e828058c2bda4b630a65a02e6b8
unknown <miccicano@gmail.com>
2018-08-15 01:36:42 -0600

    edicion en blade para cursos catedratico

--- resources/views/ProfessorCourse.blade.php
+++ resources/views/ProfessorCourse.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.app')
 
 @section('title', 'CatedraticoCursos')
+<div style="background-image: url(img/header.jpg); height: 1250px; width: 1400px;">
+	<br><br><br><br>
 
+
+{!! Form::open(['url' => '/teacher_form/evaluate', 'method' => 'post', 'class' => 'form-row align-items-center','onsubmit' => 'return confirm("Quieres enviar esta evaluacion?")']) !!}
+		<div class="container" type="text" readonly>
+		{!! Form::token('tokenN')!!}
+
+			<div class="container">
+
+
+		<div class="card">
+  		<div class="card-header">
+    		Listado Cursos Catedratico.
+  		</div>
+  		<div class="card-body">
+    		<h5 class="card-title">Evaluacion de docente</h5>
+    		<p class="card-text">Listado Cursos Catedratico.</p>
+  		</div>
+		</div>
+		<BR><BR>
+</div>
 
 @section('content')
-<h1> Catedratico <h1>
-holaaaaa
+
+
 
 @endsection
