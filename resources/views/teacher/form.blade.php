5f1fc94e816703210183a844298cbe8e79524ef0
Esteban Alvarez <davix93a@gmail.com>
2018-08-20 01:02:01 -0600

    Cambio de seleccion de lista a barra con colores JS

--- resources/views/teacher/form.blade.php
+++ resources/views/teacher/form.blade.php
@@ -2,7 +2,56 @@
 
 @section('title', 'Encuesta Profesor')
 <div style="background-image: url(img/header.jpg); height: 1250px; width: 1400px;">
-
+<style type="text/css">
+	.slider0{
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #e6e6e6;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+	}
+	.slider1{
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #00cc00;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+	}
+	.slider2{
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #ffff00;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+	}
+	.slider3{
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #cc0000;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+	}	
+	.slider0::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 25px; /* Set a specific slider handle width */
+    height: 25px; /* Slider handle height */
+    background: #4CAF50; /* Green background */
+    cursor: pointer; /* Cursor on hover */
+	}		
+</style>>
 
 
 		<br><br><br><br>
@@ -28,32 +77,42 @@
  		<!--******************************************************************-->
  		{!!Form::label('Pregunta1', '1. El catedratico cumple con el horario establecido?' ,array('class' => 'text-white'))!!}
  		<br>
-		{!!Form::select('P1', array('1' => '1', '2' => '2','3' => '3', '4' => '4','5' => '5', '6' => '6','7' => '7', '8' => '8','9' => '9', '10' => '10'), '', array('class' => 'custom-select mr-sm-2' ))!!}
-
+ 		<br>
+ 		<input type="range" min="0" max="10" value="50" class="slider0" id="R1" onchange="Barra1(this)">
+ 		<br>
+ 		<br>
 
  		<!--******************************************************************-->
  		{!!Form::label('Pregunta2', '2. El catedratico demuestra su dominio sobre los temas impartidos?',array('class' => 'text-white'))!!}
  		<br>
-		{!!Form::select('P2', array('1' => '1', '2' => '2','3' => '3', '4' => '4','5' => '5', '6' => '6','7' => '7', '8' => '8','9' => '9', '10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
- 	
+ 		<br>
+ 		<input type="range" min="0" max="10" value="50" class="slider0" id="R2" onchange="Barra1(this)">
+ 		<br>
+ 	 	<br>
 
  		<!--******************************************************************-->
  		{!!Form::label('Pregunta3', '3.La presentacion personal del catedratico es la adecuada?',array('class' => 'text-white'))!!}
  		<br>
-		{!!Form::select('P3', array('1' => '1', '2' => '2','3' => '3', '4' => '4','5' => '5', '6' => '6','7' => '7', '8' => '8','9' => '9', '10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
- 
+ 		<br>
+ 		<input type="range" min="0" max="10" value="50" class="slider0" id="R3" onchange="Barra1(this)">
+ 		<br>
+  		<br>
 
 
  		<!--******************************************************************-->
  		{!!Form::label('Pregunta4', '4. El catedratico entrega notas en las fecha establecida?',array('class' => 'text-white'))!!}
  		<br>
-		{!!Form::select('P4', array('1' => '1', '2' => '2','3' => '3', '4' => '4','5' => '5', '6' => '6','7' => '7', '8' => '8','9' => '9', '10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
-			
+ 		<br>
+ 		<input type="range" min="0" max="10" value="50" class="slider0" id="R4" onchange="Barra1(this)">
+ 		<br>
+ 		<br> 		
 <!--******************************************************************-->
  		{!!Form::label('Pregunta5', '5. El catedratico asiste al curso los dias establecidos?',array('class' => 'text-white'))!!}
  		<br>
-		{!!Form::select('P5', array('1' => '1', '2' => '2','3' => '3', '4' => '4','5' => '5', '6' => '6','7' => '7', '8' => '8','9' => '9', '10' => '10'), 'null', array('class' => 'custom-select mr-sm-2' ))!!}
-
+ 		<br>
+ 		<input type="range" min="0" max="10" value="50" class="slider0" id="R5" onchange="Barra1(this)">
+ 		<br>
+ 		<br>
 
  		{!!Form::label('Comentario', 'Agregue un comentario (Opcional).',array('class' => 'text-white'))!!}
  		<br>
@@ -64,7 +123,22 @@
  			{!!Form::submit('Evaluar', array('class'=>'btn btn-primary btn-lg btn-block'))!!}
  		</div>
 		{!! Form::close() !!}
-	</div>
+		</div>
+
+ 		<script>
+// 			var Rango1 = document.getElementById("R1");
+ 			function Barra1(Rango){
+ 				console.log(Rango.value);
+ 			if(Rango.value <= 4){
+ 					Rango.className = "slider3"; 
+ 			}else if(Rango.value > 4 && Rango.value <= 7){
+ 					Rango.className = "slider2";
+ 			}else if(Rango.value > 7){
+ 					Rango.className = "slider1";
+ 			}}
+		 			
+ 		</script>
+
 	</div>
 
 @section('content')
