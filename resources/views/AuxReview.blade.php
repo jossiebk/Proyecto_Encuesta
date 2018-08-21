4f321141515175bb05c1b7ff16517eda3573b22a
Marellyn <mlis.trejo@gmail.com>
2018-08-20 22:05:07 -0600

    interfaz terminada para opinion de auxiliares

--- resources/views/AuxReview.blade.php
+++ resources/views/AuxReview.blade.php
@@ -10,8 +10,12 @@
   <br><br>
   <form method="post" action="{{ url('/AuxReview') }}">
   @csrf
-  <div><h4>Catedratico: ################## </h4>  </div>
-  <div><textarea type="text" name="opinion" placeholder="comentario" style="width:700px; height:100px"></textarea></div>
+  <div>
+    <h4>Auxiliar: ################## </h4>  </div>
+  <div>
+    <textarea type="text" name="opinion" placeholder="comentario" style="width:600px; height:200px"></textarea>
+    <textarea type="text" name="opinion2" placeholder="comentario" style="width:600px; height:200px"></textarea>
+  </div>
   <br><br>
   <div><input type="text" name="score" placeholder="punteo"></input></div>
    <div><input type="number" name="id" placeholder="id"></input></div>
