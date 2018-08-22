a391d1eb39b52af599064ac5292ac4a8a9d11b2a
unknown <miccicano@gmail.com>
2018-08-22 09:44:36 -0600

    Modificacion de controlador Reports para funcion Top10General

--- app/Http/Controllers/Reports.php
+++ app/Http/Controllers/Reports.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class Reports extends Controller
 {
-    
+    public function Top10General(){
+    		$consulta = \DB::table('reviews as r')
+		    	->join('teachers as t', 'r.id', '=', 't.id')
+		    	->join('users u', 'u.id', '=', 't.id_user')
+		    	->select('u.name as NAME,', 'r.notes as NOTE')
+		    	->orderBy('r.notes desc')
+		    	->limit('10')
+		    	->get();
+
+		    return view('/Reports/Top10General', [
+    		'consulta' => $consulta
+    		]);
+    	
+    }
 }
