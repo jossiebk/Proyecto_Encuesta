b25207044c4884070329c55f0759f9c970ee6adc
salaz310 <lrso.10@hotmail.com>
2018-08-23 12:58:01 -0500

    Se agrego CSS para mostrar la informacion del auxiliar

--- resources/views/AssistantProfile.blade.php
+++ resources/views/AssistantProfile.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('cssextra')
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+@endsection
 
 @section('boton')
 <li class="nav-item">
@@ -13,42 +17,55 @@
 <br>
 <br>
 <br>
-		
+		<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 	  <div class="row align-items-center justify-content-center">
           <h1 style="color:white;" class="display-4">{{ $assistant->user->name }}</h1>
       </div>
+      <br>
+      <br>
+      <br>
+    <div class="container">
+      <table class="w3-table-all">
+  
+  <tbody>
+    <tr class="w3-light-grey w3-hover-red">
+      <td colspan="1">Descripcion</td>
+      <td colspan="3">{{$assistant->personal_description}}</td>
+    </tr>
 
-      <div class="container">
-  <h2>Hoverable Dark Table</h2>
-              
-  <table class="table table-dark table-hover">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <tr class="w3-hover-green">
+      <td colspan="1">Cumpleaños</td>
+      <td colspan="3">{{$assistant->birthdate}}</td>
+    </tr>
 
+    <tr class="w3-hover-blue">
+      <td colspan="1">Experiencia academica</td>
+      <td>{{$assistant->academic_experience_1}}</td>
+      <td>{{$assistant->academic_experience_2}}</td>
+      <td>{{$assistant->academic_experience_3}}</td>
+    </tr>
+
+    <tr class="w3-hover-black">
+      <td colspan="1">Experiencia laboral</td>
+      <td>{{$assistant->work_experience_1}}</td>
+      <td>{{$assistant->work_experience_2}}</td>
+      <td>{{$assistant->work_experience_3}}</td>
+    </tr>
+
+    <tr class="w3-hover-yellow">
+      <td colspan="1">Referencias</td>
+      <td colspan="3">{{$assistant->references}}</td>
+    </tr>
+
+  </tbody>
+</table>
+
+    </div>
 
 </div>
 @endsection
