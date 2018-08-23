137dcf20239ee7dc9f825dcf459c14d410a6e4e7
unknown <miccicano@gmail.com>
2018-08-23 00:10:35 -0600

    edicion de vista para mostrar datos del reporte

--- resources/views/Reports/Top10General.blade.php
+++ resources/views/Reports/Top10General.blade.php
@@ -2,26 +2,39 @@
 
 @section('title', 'Top10General')
 <div style="background-image: url(/img/header.jpg); height: 1250px; width: 1400px;">
-	<br><br><br><br>
+  <br><br><br><br>
+
+    <div class="container" type="text" readonly>
+
+      <div class="container">
 
 
-{!! Form::open(['url' => '/teacher_form/evaluate', 'method' => 'post', 'class' => 'form-row align-items-center','onsubmit' => 'return confirm("¿Quiere salir de este reporte?")']) !!}
-		<div class="container" type="text" readonly>
-		{!! Form::token('tokenN')!!}
-
-			<div class="container">
-
-
-		<div class="card">
-  		<div class="card-header">
-    		Top 10 Catedraticos en General
-  		</div>
-  		<div class="card-body">
-    		<h5 class="card-title">Top 10 Catedraticos en General</h5>
-  		</div>
-
-		</div>
-	<BR><BR>
+    <div class="card">
+      <div class="card-header">
+        Top 10 Catedraticos en General
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Los 10 catedraticos mejor calificados son:</h5>
+      </div>
+          @if(isset($consulta) && count($consulta) > 0)
+            <center>
+              <table>
+                <tr>
+                <th> NAME &nbsp;&nbsp;&nbsp;</th>
+                <th> &nbsp; &nbsp; NOTE &nbsp;</th>
+                </tr>
+                @foreach($consulta as $registro)
+                <tr>
+                <td>{{ $registro->NAME }} &nbsp; &nbsp; &nbsp;</td>
+                <td> &nbsp; &nbsp;{{ $registro->NOTE }}</td>
+                </tr>
+                @endforeach
+              </table>
+            </center>
+          @endif
+       <BR><BR>
+    </div>
+  <BR><BR>
 </div>
 
 
