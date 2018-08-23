42636be16e9178217c6a83370f558652d97c751f
salaz310 <lrso.10@hotmail.com>
2018-08-22 23:29:30 -0500

    Base para la muestra del perfil del Auxiliar

--- resources/views/AssistantProfile.blade.php
+++ resources/views/AssistantProfile.blade.php
@@ -18,6 +18,37 @@
           <h1 style="color:white;" class="display-4">{{ $assistant->user->name }}</h1>
       </div>
 
+      <div class="container">
+  <h2>Hoverable Dark Table</h2>
+              
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
 
 </div>
 @endsection
