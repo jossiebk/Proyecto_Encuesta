8d1c1a72e5e68592dc7acf0515153399784c5e7c
Diego Momotic <diegomomotic@gmail.com>
2018-09-07 12:46:31 -0600

    Se codifico para que las pruebas pasen, con los escenarios ver opcion de busqueda de catedraticos, opcion para listar auxiliares y opcion para listar catedraticos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,41 @@
     <div class="container text-center">
       <h2 class="mb-4 sr-button">Dashboard Estudiantes</h2>
     </div>
+
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#search">
+                <i class="fa fa-4x fa-address-card text-secondary mb-3 sr-icons"></i>
+                <p class="text-muted mb-0">Buscar catedratico o auxiliar</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#">
+                <i class="fa fa-4x fa-user-friends text-secondary mb-3 sr-icons"></i>
+                <p class="text-muted mb-0">Listar auxiliares</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#">
+                <i class="fa fa-4x fa-user-graduate text-secondary mb-3 sr-icons"></i>
+                <p class="text-muted mb-0">Listar catedraticos</p>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
   </section>
 
 @include('blocks.search')
