f8c5e3dba01e24e73a9598e3cbd61435abb2fb73
Diego Momotic <diegomomotic@gmail.com>
2018-09-06 22:29:56 -0600

    codificacion para escenario que muestra el titulo del dashboard de estudiantes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,79 +9,12 @@
 
 
 @section('content')
-<div style="background-image: url(img/header.jpg); height: 1250px; width: 1400px;">
+  <section class="bg-dark text-white">
+    <div class="container text-center">
+      <h2 class="mb-4 sr-button">Dashboard Estudiantes</h2>
+    </div>
+  </section>
 
-        
+@include('blocks.search')
 
-
-      <div class="row align-items-center justify-content-center">
-          <h1 style="color:transparent;" class="display-1">e</h1>
-      </div>
-      <div class="row align-items-center justify-content-center">
-          <h1 style="color:white;" class="display-4">Bienvenido</h1>
-      </div>
-
-       <div class="row align-items-center justify-content-center">
-          <div class="col-lg-4 col-sm-6 ">
-            <a class="portfolio-box nav-link js-scroll-trigger" href="#search">
-              <img class="img-fluid" src="img/buscar.jpg" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Buscar
-                  </div>
-                  <div class="project-name">
-                    Busca algun catedratico o auxiliar
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-     </div>
-     
-      <br>
-
-      <div class="row align-items-center justify-content-center">
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="#">
-              <img class="img-fluid" src="img/taux.jpg" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Auxiliares
-                  </div>
-                  <div class="project-name">
-                    Ver el listado de auxiliares de catedra
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-      </div>
-      
-      <br>
-
-<div class="row align-items-center justify-content-center">
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="#">
-              <img class="img-fluid" src="img/teachers.jpg" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Catedraticos
-                  </div>
-                  <div class="project-name">
-                    Ver el listado de catedraticos
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-      </div>
-
-     <br>
-
-     @include('blocks.search')
-
- </div>
 @endsection
