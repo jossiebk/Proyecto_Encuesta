b09a6ac21658fc9753717a235e537952fe98744e
salaz310 <lrso.10@hotmail.com>
2018-09-07 23:55:14 -0500

    Se cambio el estilo de mostrar los datos en el perfil de catedratico

--- resources/views/catProfile.blade.php
+++ resources/views/catProfile.blade.php
@@ -1,45 +1,97 @@
 @extends('layouts.app')
 
+@section('cssextra')
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+@endsection
 
-@section('title', 'Perfil de Catedratico')
-<div style="background-image: url('/img/header.jpg'); height: 1250px; width: 1600px;">
+@section('boton')
+<li class="nav-item">
+<a class="nav-link js-scroll-trigger" href="#">Cerrar Sesion</a>
+</li>
+@endsection
 
 @section('content')
-	<br><br><br><br><br><br>
-	<div class="row align-items-center justify-content-center">
+<div style="background-image: url(/img/header.jpg); height: 1250px; width: 1400px;">
+<br>
+    <br>
+    <br>
+    <br>
+    <br>
+<div class="row align-items-center justify-content-center">
           <h1 style="color:white;" class="display-3">Perfil de catedratico</h1>
       </div>
-	<br><br>
-	<div><h4> Nombre:  {{ $teacher->user->name }} </h4>  </div>
-	<div><h4> Fecha de Nacimiento: {{ $teacher->birthdate }} </h4></div>
-	<div><h4> Descripcion: {{$teacher->personal_description}}</h4></div>
-	<div><h4> Titulos: 1. {{$teacher->academic_title_1}}</h4></div>
-	<div><h4> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2. {{$teacher->academic_title_2}}</h4></div>
-	<div><h4> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3. {{$teacher->academic_title_3}}</h4></div>
-	<div><h4> Experiencia: 1. {{$teacher->work_experience_1}}</h4></div>
-	<div><h4>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. {{$teacher->work_experience_1}}</h4></div>
-	<div><h4>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3. {{$teacher->work_experience_1}}</h4></div>
-	<div><h4> Referencias: {{$teacher->references}}</h4></div>
-		<br><br><br>
-	<div>
-		<center>
-		<table>
-			<tr>
+    <br>
 
-				<td><button style="width:250px; height:40px"><a  href="{{url('teacher_form',['id'=>$teacher->id]) }}";>Encuesta</a></button></td>
+	  <div class="row align-items-center justify-content-center">
+          <h1 style="color:white;" class="display-3">{{ $teacher->user->name }}</h1>
+      </div>
+      <br>
+      <br>
+      <br>
+    <div class="container">
+      <table class="w3-table-all">
+  
+  <tbody>
+    <tr class="w3-light-grey w3-hover-red">
+      <td colspan="1">Descripcion</td>
+      <td colspan="3">{{$teacher->personal_description}}</td>
+    </tr>
 
-				<td><button style="width:250px; height:40px"><a  href="{{url('teacherReview',['id'=>$teacher->id]) }}"; >Opinar</a></button></td>
-				
-				<td><button style="width:250px; height:40px"><a  href="{{url('ProfileCourse',['id'=>$teacher->id]) }}";>Cursos</a></button></td>
+    <tr class="w3-hover-green">
+      <td colspan="1">Cumpleaños</td>
+      <td colspan="3">{{$teacher->birthdate}}</td>
+    </tr>
 
-			</tr>
-		</table>
-	</center>
-	</div>
+    <tr class="w3-hover-blue">
+      <td colspan="1">Titulos</td>
+      <td>{{$teacher->academic_title_1}}</td>
+      <td>{{$teacher->academic_title_2}}</td>
+      <td>{{$teacher->academic_title_3}}</td>
+    </tr>
+
+    <tr class="w3-hover-black">
+      <td colspan="1">Experiencia laboral</td>
+      <td>{{$teacher->work_experience_1}}</td>
+      <td>{{$teacher->work_experience_2}}</td>
+      <td>{{$teacher->work_experience_3}}</td>
+    </tr>
+
+    <tr class="w3-hover-brown">
+      <td colspan="1">Referencias</td>
+      <td colspan="3">{{$teacher->references}}</td>
+    </tr>
+
+  </tbody>
+</table>
+
+
+<br>
+<div class="row">
+
+
+ <div class="col-sm-4">
+ <a href="{{url('teacher_form',['id'=>$teacher->id]) }}" 
+ 	class="btn btn-success btn-block" 
+ 	role="button" >Encuesta</a>
+ </div>
+
+
+<div class="col-sm-4">
+ <a href="{{url('teacherReview',['id'=>$teacher->id]) }}" 
+ 	class="btn btn-info btn-block" 
+ 	role="button" >Opinar</a>
+ </div>
+
+<div class="col-sm-4">
+ <a href="{{url('ProfileCourse',['id'=>$teacher->id]) }}" 
+ 	class="btn btn-warning btn-block" 
+ 	role="button" >Cursos</a>
+ </div>
 
 </div>
-
-
+</div>
+</div>
 @endsection
 
 
