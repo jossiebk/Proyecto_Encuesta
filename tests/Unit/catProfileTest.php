d522a282046af993fdc4966cbb97d333a22c0911
Diego Momotic <diegomomotic@gmail.com>
2018-09-10 17:39:23 -0600

    Correciones de errores para paso de la prueba catProfileTest

--- tests/Unit/catProfileTest.php
+++ tests/Unit/catProfileTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class catProfile extends TestCase
+class catProfileTest extends TestCase
 {
 	 /**
      * A basic test example.
