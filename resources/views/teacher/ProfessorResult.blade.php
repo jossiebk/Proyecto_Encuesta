d800cf8b2d0ec07d4509cf1ff09e38e99bdbc768
unknown <miccicano@gmail.com>
2018-09-13 15:35:05 -0600

    se agrego plantilla en vista

--- resources/views/teacher/ProfessorResult.blade.php
+++ resources/views/teacher/ProfessorResult.blade.php
@@ -1,6 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'CatedraticoCursos')
+<div style="background-image: url(/img/header.jpg); height: 1250px; width: 1400px;">
+	<br><br><br><br>
+
+
+{!! Form::open(['url' => '/teacher_form/evaluate', 'method' => 'post', 'class' => 'form-row align-items-center','onsubmit' => 'return confirm("Quieres enviar esta evaluacion?")']) !!}
+		<div class="container" type="text" readonly>
+		{!! Form::token('tokenN')!!}
+
+			<div class="container">
+
+
+		<div class="card">
+  		<div class="card-header">
+    		Notas obtenidas por Curso:
+  		</div>
+  		<div class="card-body">
+    		<h5 class="card-title">Evaluacion de docente</h5>
+  		</div>
+
+
 @if(isset($consulta) && count($consulta) > 0)
 <center>
-<h3>Nota obtenida por curso:</h3>
 <table>
 <tr>
 <th> CURSO &nbsp;</th>
@@ -18,3 +40,21 @@
 @else
 <h3>No se encontró ningún curso para calificar </h3>
 @endif
+
+	</div>
+		<BR><BR>
+
+
+
+
+
+
+</div>
+
+
+
+@section('content')
+
+
+
+@endsection
