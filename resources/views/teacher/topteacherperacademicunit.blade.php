4277a043d1e91ea7f7af357ee57e1ae20e6a6214
Esteban Alvarez <davix93a@gmail.com>
2018-09-12 21:53:39 -0600

    TDD, indicador de select para unidad academica, prueba exitosa

--- resources/views/teacher/topteacherperacademicunit.blade.php
+++ resources/views/teacher/topteacherperacademicunit.blade.php
@@ -11,7 +11,22 @@
       <div class="container text-center">
         <h2 class="section-heading text-white">TOP 10 CATEDRATICOS POR UNIDAD ACADEMICA</h2>
         <hr class="light my-4">
-        
+         <p class="text-faded mb-4">Seleccione el nombre de la Unidad Academica desee consultar</p>
+        <form method="GET" action="{{ url('/Generate_Top') }}">
+          @csrf
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">Unidad Academica:</span>
+            </div>
+				<select class="form-control form-control-lg" name="school_name">
+
+				</select>
+			</div>
+			
+				<br>
+				<br>
+          <input class="btn btn-light btn-xl" type="submit" value="Buscar!"></input>
+        </form>
 		</div>
 		</div>
 @endsection
