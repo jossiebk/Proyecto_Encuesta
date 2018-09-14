609676dc90f11153247f0a16ea2d74a16729217a
Marellyn <mlis.trejo@gmail.com>
2018-09-13 18:40:12 -0600

    TDD- siguiente prueba unitaria realizada sin aprobar

--- tests/Unit/Top10APSTest.php
+++ tests/Unit/Top10APSTest.php
@@ -41,4 +41,16 @@ class Top10APSTest extends TestCase
         $response->assertSee('TOP 10 AUXILIARES POR UNIDAD ACADEMICA');
         $response->assertSuccessful();
     }
+
+     /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function generate_top_nothing()
+    {
+        $response = $this->json('GET',  '/Top10AssistantPerAcademicUnit/Top_Generado', ['school_name' => 'Sincere Collier']);
+        $response->assertSee('Resultados de Reporte');
+        $response->assertSuccessful();
+    } 
 }
