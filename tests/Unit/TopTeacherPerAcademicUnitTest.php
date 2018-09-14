1de1c15bb13e36de6fb51e2b954a7ab711471b2b
Esteban Alvarez <davix93a@gmail.com>
2018-09-13 21:28:15 -0600

    TDD, generacion y visualizacion de reporte basado en unidad academica, prueba fallida

--- tests/Unit/TopTeacherPerAcademicUnitTest.php
+++ tests/Unit/TopTeacherPerAcademicUnitTest.php
@@ -61,4 +61,34 @@ class TopTeacherPerAcademicUnitTest extends TestCase
     }    
 
 
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function generate_top_nothing()
+    {
+        $response = $this->json('GET',  '/Generate_TopAU', ['unit_name' => 'Ciencias']);
+        $response->assertViewIs('teacher.show_topau');
+        $response->assertSee('Resultados de Reporte:');
+        $response->assertSee('Reporte No Generado');
+        $response->assertSuccessful();
+    }    
+
+  
+     /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function generate_top_generated()
+    {
+        $response = $this->json('GET',  '/Generate_TopAU', ['unit_name' => 'King Beer I']);
+        $response->assertViewIs('teacher.show_topau');
+        $response->assertSee('Reporte Top Catedraticos por Unidad Academica');
+        $response->assertSee('Resultados de Reporte:');
+        $response->assertSuccessful();
+    }  
+
+
 }
