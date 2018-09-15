e2afa3f31484fc1077251ee253a9135f3c52685a
unknown <miccicano@gmail.com>
2018-09-14 18:37:24 -0600

    cambio en prueba unitaria

--- tests/Unit/ProfessorResultTest.php
+++ tests/Unit/ProfessorResultTest.php
@@ -18,4 +18,28 @@ class ProfessorResultTest extends TestCase
     	$this->get('/teacher/ProfessorResult/15')
     		->assertStatus(200);
     }
+
+     public function if_serch_professor_course_exist()
+    {
+        $response = $this->json('GET',  '/teacher/ProfessorResult/15');
+        $response->assertViewIs('ProfessorCourse');
+        if ($response->assertSee('Notas obtenidas por Curso:')){
+        	$response->assertSuccessful();
+        }
+    }
+
+
+    /**
+     * A basic test
+     * @test
+     * @return void
+     */
+    public function if_serch_professor_course_not_exist()
+    {
+        $response = $this->json('GET',  '/teacher/ProfessorResult/15lkji');
+        $response->assertViewIs('ProfessorCourse');
+        if ($response->assertSee('')){
+        	$response->assertSuccessful();
+        }
+    }
 }
