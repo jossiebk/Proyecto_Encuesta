1a4a2ed89751c269b3c4b54a8ad04f1b309801d6
Marellyn <mlis.trejo@gmail.com>
2018-10-02 19:21:57 -0600

    Se cre el controlador para la historia de usuario de cargar auxiliares

---
+++ resources/views/assistants/LoadAssistant.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Pagina de Inicio')
+
+@section('content')
+  <header class="masthead text-center text-white d-flex">
+     
+      <div class="container my-auto">
+
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>Carga auxiliares</strong>
+            </h1>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </header>
+@endsection
