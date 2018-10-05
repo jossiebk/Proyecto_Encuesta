0fc20ab437d7914f2241ee7d7814ebb20abbc322
Marellyn <mlis.trejo@gmail.com>
2018-10-04 22:58:26 -0600

    Carga a la base de datos del curiculum de auxiliar

--- app/Http/Controllers/LoadAssistantController.php
+++ app/Http/Controllers/LoadAssistantController.php
@@ -74,7 +74,22 @@ class LoadAssistantController extends Controller
     public function update(Request $request)
     {
         //
-            dd($request);
+           // dd($request);
+            $r = new Assistant();
+            $r->birthdate = $request->input('birthdate');
+            $r->image = $request->input('image');
+            $r->credits = $request->input('credits');;
+            $r->personal_description = $request->input('personal_description');
+            $r->academic_experience_1 = $request->input('academic_experience_1');
+            $r->academic_experience_2 = $request->input('academic_experience_2');
+            $r->academic_experience_3 = $request->input('academic_experience_3');
+            $r->work_experience_1 = $request->input('work_experience_1');
+            $r->work_experience_2 = $request->input('work_experience_2');
+            $r->work_experience_3 = $request->input('work_experience_3');
+            $r->references = $request->input('references');
+            $r->id_user = 10;
+            $r->save();
+            return redirect('/');
 
     }
 
