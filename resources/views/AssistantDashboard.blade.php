7795d95961d2009004ee9d9b5ebc7d2d02c9cb75
salaz310 <lrso.10@hotmail.com>
2018-10-05 16:35:24 -0500

    Se redirecciono el cargar curriculum y terminaron de pasar las pruebas

--- resources/views/AssistantDashboard.blade.php
+++ resources/views/AssistantDashboard.blade.php
@@ -43,7 +43,7 @@
           <div class="col-lg-4 col-md-4 text-center">
             <div class="service-box mt-5 mx-auto">
               
-              <a href="#">
+              <a href="LoadAssistant/{{Auth::user()->assistant->id}}">
                 <i class="fa fa-4x fa-user-circle text-secondary mb-3 sr-icons"></i>
                 <p class="text-muted mb-0">Cargar Curriculum</p>
               </a>
