8016ccd01c17800bfc10a48e3e3953d41999f28b
Diego Momotic <diegomomotic@gmail.com>
2018-10-05 21:58:16 -0600

    Se realizaron las pruebas de aceptacion, se cambio de vista las opciones de editar informacion del auxiliar y se separo la logica en el controlador

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,8 @@ Route::post('/teacher/dashboard/reference', 'TeacherController@reference');
 // ruta para perfil del auxiliar
 Route::get('/assistantProfile/{id}', 'AssistantController@profile');
 
-Route::post('/assistantProfile', 'AssistantController@title_work');
+Route::post('/assistantProfile/academic', 'AssistantController@title');
+Route::post('/assistantProfile/work', 'AssistantController@work');
 
 Route::get('/AuxReview/{id}', 'AssistantController@reviewAux');
 
